837f47e4fc953154df29ef1982240bb59f0ff9a0
Raj-kansagra <rajkansagra888@gmail.com>
2024-09-13 19:22:59 +0530

    tested env

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-
+dotenv.config();
 // Define a simple GET endpoint
-app.get('/', (req, res) => {
-    res.json({message: 'Hello Developers'});
-  });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Developers", envir: process.env.NAME });
+});
 
-
-
-app.get('/test', (req, res) => {
+app.get("/test", (req, res) => {
   res.json({
-    message: 'Hello from Express!',
-    status: 'success'
+    message: "Hello from Express!",
+    status: "success",
   });
 });
 
